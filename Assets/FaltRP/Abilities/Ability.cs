62e8c0de75814b5568df211bda59ec3671ff9745
Arcfalt <me@arcfalt.com>
2017-05-02 21:37:59 +1000

    Added the basics for items and abilities

---
+++ Assets/FaltRP/Abilities/Ability.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace FaltRP.Abilities
+{
+	/// <summary>
+	/// Container for a usable ability
+	/// </summary>
+	[CreateAssetMenu(fileName = "New Ability", menuName = "ArcRP/Ability", order = 1002)]
+	public class Ability : ScriptableObject
+	{
+		/// <summary>
+		/// Action point cost of the ability
+		/// </summary>
+		[SerializeField]
+		private int _apCost;
+	}
+}
---
+++ Assets/FaltRP/Abilities/AbilityRef.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace FaltRP.Abilities
+{
+	/// <summary>
+	/// Reference to an ability
+	/// </summary>
+	[Serializable]
+	public class AbilityRef
+	{
+		/// <summary>
+		/// ID of the ability to reference
+		/// </summary>
+		[SerializeField]
+		private string _abilityId;
+
+		private Ability _abilityRef;
+
+		public void FindReference()
+		{
+			// Todo - Find the ability in the loaded campaign data by ID
+		}
+	}
+}
